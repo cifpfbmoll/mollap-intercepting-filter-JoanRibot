02eb0a8d4e6d8102ad834f460c93f43498ab4b77
JoanRibot <jribot@cifpfbmoll.eu>
2021-04-28 19:32:55 +0200

    filters ok

---
+++ mollap/src/main/java/intercepting/App.java
@@ -0,0 +1,36 @@
+package intercepting;
+
+import intercepting.administrador.ProgramadorTasques;
+import intercepting.clients.Mollapp;
+import intercepting.filtres.Autoritzacio;
+import intercepting.targets.Vehicle;
+import intercepting.filtres.Autenticacio;
+
+public final class App {
+    public static void main(String[] args) {
+
+        /**
+         * Configuració del programador de tasques
+         * del sistema amb el tipus de target triat:
+         * des de vehicles a qualsevol cosa que admiteix
+         * la recepció d'un missatge.
+         */
+        ProgramadorTasques programadorTasques = new ProgramadorTasques(new Vehicle());
+
+        /**
+         * Afegir al sistema les tasques que volem que el sistema
+         * executi al rebre la petició del client.
+         */
+        programadorTasques.setTasca(new Autenticacio());
+        programadorTasques.setTasca(new Autoritzacio());
+
+        /**
+         * Configuració de l'app client per a que
+         * executi les tasques programades i
+         * enviï el misstage al sistema.
+         */
+        Mollapp mollapp = new Mollapp();
+        mollapp.setProgramadorTasques(programadorTasques);
+        mollapp.enviarPeticio("Francesc");
+    }
+}
